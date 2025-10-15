d9cdfeb124fbee102c47457d2c207b0fa5a8080c
Makis Papadopoulos <makpapad@gmail.com>
2025-10-15 14:11:13 +0300

    Remove optimizeCss experiment to fix critters module error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
-  images: { domains: ['app.billiardtoday.com'] },
-  experimental: { optimizeCss: true }
+  images: { domains: ['app.billiardtoday.com'] }
 };
 module.exports = nextConfig;
