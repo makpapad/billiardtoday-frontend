e134497db5f622b8914d549137215b28d9d7eb81
Makis Papadopoulos <makpapad@gmail.com>
2026-03-16 16:26:38 +0200

    Keep claim and enroll flow standalone

--- src/app/claim/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { AppSiteShell } from "@/components/site/AppSiteShell";
-
-export default async function ClaimLayout({ children }: { children: React.ReactNode }) {
-  return <AppSiteShell>{children}</AppSiteShell>;
-}
--- src/app/enroll/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { AppSiteShell } from "@/components/site/AppSiteShell";
-
-export default async function EnrollLayout({ children }: { children: React.ReactNode }) {
-  return <AppSiteShell>{children}</AppSiteShell>;
-}
