1875be9d22649a98bfdc8be56c076893485b79fe
Makis Papadopoulos <makpapad@gmail.com>
2026-05-01 07:32:02 +0300

    Use direct GA4 tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,8 @@ import { getCmsAppearance } from "@/lib/cms/strapi";
 
 export const dynamic = "force-dynamic";
 
-const googleTagManagerId = process.env.NEXT_PUBLIC_GTM_ID || "GTM-M2LPF82V";
+const googleAnalyticsId =
+  process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "G-BHBG58ND1R";
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://billiardtoday.com"),
@@ -86,29 +87,24 @@ export default async function RootLayout({
   return (
     <html lang="el" className={htmlClassName} suppressHydrationWarning>
       <head>
-        {googleTagManagerId ? (
+        {googleAnalyticsId ? (
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+          />
+        ) : null}
+        {googleAnalyticsId ? (
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${googleTagManagerId}');`,
+              __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${googleAnalyticsId}');`,
             }}
           />
         ) : null}
       </head>
       <body className="min-h-screen flex flex-col" style={bodyStyle}>
-        {googleTagManagerId ? (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
-        ) : null}
         <script dangerouslySetInnerHTML={{ __html: colorModeScript }} />
         <AuthProvider>{children}</AuthProvider>
       </body>
